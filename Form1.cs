8d54349a0df8354cb984ce23c523e2295595b63c
Ahmad <86000820+AhmadRaza56@users.noreply.github.com>
2022-04-20 10:39:28 +0100

    establishe connection to mysql

--- Form1.cs
+++ Form1.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +17,17 @@ namespace connect_to_yugioh_database
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connetionString;
+            MySqlConnection cnn;
+            connetionString = @"Data Source=localhost;Initial Catalog=mydb;User ID=root;Password=root";
+            cnn = new MySqlConnection(connetionString);
+            cnn.Open();
+            MessageBox.Show("Connection Open  !");
+            cnn.Close();
         }
     }
 }
